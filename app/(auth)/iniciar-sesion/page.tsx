ff2fdd595f8479f4f4d14a9aae13abf3b973d1cf
jaycodev <jason.vilac@gmail.com>
2025-12-20 13:11:19 -0500

    refactor(auth): streamline login flow and improve middleware redirection

--- app/(auth)/iniciar-sesion/page.tsx
+++ app/(auth)/iniciar-sesion/page.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from 'next'
+import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
 
+import { RedirectIfCookie } from '@auth/components/redirect-if-cookie'
 import { LoginPage } from '@auth/pages/login'
 
 import { pageMap } from '@/config/page-map'
@@ -10,6 +13,17 @@ export const metadata: Metadata = {
   title: page.title,
 }
 
-export default function Page() {
-  return <LoginPage />
+export default async function Page() {
+  const cookieStore = await cookies()
+  const token = cookieStore.get('access_token')
+  if (token?.value) {
+    redirect('/admin')
+  }
+
+  return (
+    <>
+      <RedirectIfCookie />
+      <LoginPage />
+    </>
+  )
 }
